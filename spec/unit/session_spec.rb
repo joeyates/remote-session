f26a17f917a7c17bd27671b0ae72734bb0a3770d
Joe Yates <joe.g.yates@gmail.com>
2012-05-17 16:46:19 +0100

    Specs missed from previous commit

---
+++ spec/unit/session_spec.rb
@@ -0,0 +1,301 @@
+# encoding: utf-8
+load File.expand_path( '../spec_helper.rb', File.dirname(__FILE__) )
+
+describe Remote::Session do
+
+  TEST_HOST = 'host.example.com'
+
+  context 'initialization' do
+
+    before :each do
+      Net::SSH.stub!( :start )
+      @user         = ENV[ 'USER' ]
+      ENV[ 'USER' ] = 'the_user'
+    end
+
+    after :each do
+      ENV[ 'USER' ] = @user
+    end
+
+    it 'should require a hostname parameter' do
+      expect do
+        Remote::Session.new
+      end.       to         raise_error( ArgumentError, 'wrong number of arguments (0 for 1)' )
+    end
+
+    it 'should connect automatically' do
+      Net::SSH.should_receive( :start )
+
+      Remote::Session.new( TEST_HOST )
+    end
+
+    it 'user should default to the current user' do
+      Net::SSH.should_receive( :start ).with( TEST_HOST, 'the_user', {} )
+
+      Remote::Session.new( TEST_HOST )
+    end
+
+    it 'should accept user option' do
+      Net::SSH.should_receive( :start ).with( TEST_HOST, 'another_user', {} )
+
+      Remote::Session.new( TEST_HOST, :user => 'another_user' )
+    end
+
+    it 'should use any supplied password' do
+      Net::SSH.should_receive( :start ).with( TEST_HOST, 'another_user', { :password => 'secret' } )
+
+      Remote::Session.new( TEST_HOST, :user => 'another_user', :password => 'secret' )
+    end
+
+  end
+
+  context 'instance methods' do
+
+    before :each do
+      @ssh = stub( 'Net::SSH instance' )
+      Net::SSH.stub!( :start => @ssh )
+    end
+
+    context '#open' do
+
+      it 'should run the block' do
+        @ssh.stub!( :close )
+
+        called = false
+        Remote::Session.open( TEST_HOST, :user => 'another_user' ) do | rs |
+          called = true
+        end
+
+        called.should be_true
+      end
+
+      it 'should open a connection' do
+        @ssh.stub!( :close )
+
+        Net::SSH.should_receive( :start ).with( 'host.example.com', 'another_user', {} )
+
+        Remote::Session.open( TEST_HOST, :user => 'another_user' ) {}
+      end
+
+      it 'should require a block' do
+        expect do
+          Remote::Session.open( TEST_HOST, :user => 'another_user' )
+        end.       to         raise_error( NoMethodError, "undefined method `call' for nil:NilClass" )
+      end
+
+      it 'should close the connection' do
+        @ssh.should_receive( :close )
+
+        Remote::Session.open( TEST_HOST, :user => 'another_user' ) {}
+      end
+
+    end
+
+    context '#run' do
+
+      subject { Remote::Session.new( TEST_HOST ) }
+
+      it 'should fail, if the session is closed' do
+        @ssh.stub!( :close )
+        subject.close
+
+        expect do
+          subject.run( 'pwd' )
+        end.to raise_error( RuntimeError, 'Session is closed' )
+      end
+
+      it 'should print the command to stdout' do
+        @ssh.stub!( :exec! => "/foo/bar\n" )
+
+        subject.should_receive( :puts ).with( "@#{TEST_HOST}: pwd" )
+        subject.should_receive( :puts ).with( "/foo/bar\n" )
+
+        subject.run( 'pwd' )
+      end
+
+      it 'should run the command' do
+        subject.stub!( :puts )
+
+        @ssh.should_receive( :exec! ).with( 'pwd' ).and_return( "/foo/bar\n" )
+
+        subject.run( 'pwd' )
+      end
+
+    end
+
+    context '#sudo' do
+
+      subject { Remote::Session.new( TEST_HOST ) }
+
+      it 'should fail, if the session is closed' do
+        @ssh.stub!( :close )
+        subject.close
+
+        expect do
+          subject.sudo( 'pwd' )
+        end.to raise_error( RuntimeError, 'Session is closed' )
+      end
+
+      it 'should print the command to stdout' do
+        @ssh.stub!( :open_channel => nil )
+        @ssh.stub!( :loop         => nil )
+
+        subject.should_receive( :puts ).with( "@#{TEST_HOST}: sudo pwd" )
+
+        subject.sudo( 'pwd' )
+      end
+
+      it 'should run the command' do
+        subject.stub!( :puts )
+
+        @ssh.should_receive( :open_channel ) do |&open_channel_block|
+          @channel = stub( 'channel' )
+
+          @channel.should_receive( :request_pty ) do |&request_pty_block|
+            request_pty_block.call( @channel, true )
+          end
+
+          @channel.should_receive( :exec ).with( "sudo -p 'sudo_prompt' pwd" )
+
+          open_channel_block.call @channel
+        end
+        @ssh.should_receive( :loop )
+
+        subject.sudo( 'pwd' )
+      end
+
+      context 'in channel' do
+
+        before :each do
+          @rs = Remote::Session.new( TEST_HOST, { :sudo_password => 'secret' } )
+          @rs.stub!( :puts )
+
+          @ssh.stub!( :loop => nil )
+          @ch = stub( 'channel' )
+          @ssh.stub!( :open_channel ) do |&block|
+            block.call @ch
+          end
+        end
+
+        it 'should fail if pty request is unsuccessful' do
+          @ch.stub!( :request_pty ) do |&block|
+            expect do
+              block.call( @ch, false )
+            end.to raise_error( RuntimeError, 'Could not obtain pty' )
+          end
+
+          @rs.sudo( 'pwd' )
+        end
+
+        context 'with pty' do
+
+          before :each do
+            @ch.stub!( :request_pty ) do |&block|
+              block.call( @ch, true )
+            end
+          end
+
+          it 'should fail if the command fails' do
+            @ch.stub!( :exec ) do |&block|
+              expect do
+                block.call( @ch, false )
+              end.to raise_error( RuntimeError, 'Could not execute sudo command: pwd' )
+            end
+
+            @rs.sudo( 'pwd' )
+          end
+
+          context 'in exec' do
+            before :each do
+              @ch.stub!( :exec ) do |&block|
+                block.call( @ch, true )
+              end
+              @ch.stub!( :on_extended_data => nil )
+              @ch.stub!( :send_data )
+            end
+
+            it 'should output returning data' do
+              @ch.stub!( :on_data ) do |&block|
+                block.call( @ch, 'some_data' )
+              end
+
+              @rs.should_receive( :puts ).with( 'some_data' )
+
+              @rs.sudo( 'pwd' )
+            end
+
+            context 'with password prompt' do
+              before :each do
+                @ch.stub!( :on_data ) do |&block|
+                  block.call( @ch, 'sudo_prompt' )
+                end
+              end
+
+              it 'should supply the sudo password, when prompted' do
+                @ch.should_receive( :send_data ).with( "secret\n" )
+
+                @rs.sudo( 'pwd' )
+              end
+
+              it 'should not echo the standard prompt' do
+                output = []
+                @rs.stub!( :puts ) do | s |
+                  output << s
+                end
+
+                @rs.sudo( 'pwd' )
+
+                output.should == ["@#{TEST_HOST}: sudo pwd"]
+              end
+            end
+
+            context 'with user-supplied prompt' do
+              before :each do
+                @ch.stub!( :on_data ) do |&block|
+                  block.call( @ch, 'Here is my prompt:' )
+                end
+              end
+
+              it 'should send the supplied data' do
+                @ch.should_receive( :send_data ).with( "this data\n" )
+
+                @rs.sudo( 'pwd', 'my prompt' => 'this data' )
+              end
+
+              it 'should not echo the prompt' do
+                output = []
+                @rs.stub!( :puts ) do | s |
+                  output << s
+                end
+
+                @rs.sudo( 'pwd', 'my prompt' => 'this data' )
+
+                output.should == ["@#{TEST_HOST}: sudo pwd"]
+              end
+
+            end
+
+            it 'should fail if error data is received' do
+              @ch.stub!( :on_data )
+
+              @ch.stub!( :on_extended_data ) do |&block|
+                block.call @ch, 'foo', 'It failed' 
+              end
+
+              expect do
+                @rs.sudo( 'pwd' )
+              end.to raise_error( RuntimeError, 'Error It failed while performing command: pwd' )
+            end
+
+          end
+
+        end
+
+      end
+
+    end
+
+  end
+
+end
+
