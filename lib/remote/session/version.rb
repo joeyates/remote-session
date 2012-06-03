9b51f0438cd768ed5015ee7ee2c5f3dbd64cd94f
Joe Yates <joe.g.yates@gmail.com>
2012-06-03 21:51:11 +0100

    Version bump for release

--- lib/remote/session/version.rb
+++ lib/remote/session/version.rb
@@ -2,7 +2,7 @@ module Remote
   class Session
     MAJOR    = 0
     MINOR    = 0
-    REVISION = 5
+    REVISION = 6
     VERSION  = [ MAJOR, MINOR, REVISION ].map( &:to_s ).join( '.' )
   end
 end
